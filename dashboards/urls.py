1ce71cd271cabfa5a9018de66e8f115dff929d32
Arham70 <nasirarham656@gmail.com>
2023-12-29 19:11:15 +0500

    clone unity project completed

---
+++ dashboards/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from dashboards.views import (
+    dashboard_view,
+    ProfileUpdateView,
+    NotificationListView,
+    NotificationDeleteView,
+)
+from attribute.views import AttributeListView
+
+app_name = "dashboards"
+
+urlpatterns = [
+    path("profile/<int:pk>/", ProfileUpdateView.as_view(), name="user_profile"),
+    # path("", view=dashboard_view, name="dashboard"),
+    path("", AttributeListView.as_view(), name="dashboard"),
+    path(
+        "notification-list/", NotificationListView.as_view(), name="notification_list"
+    ),
+    path(
+        "notification-delete/<int:pk>/",
+        NotificationDeleteView.as_view(),
+        name="notification_delete",
+    ),
+]
